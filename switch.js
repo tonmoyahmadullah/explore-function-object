a199f7b6dfbd724d1ac28d381095eda2a621464b
tonmoyahmadullah <tonmoyahmadullah@gmail.com>
2021-08-10 06:39:57 +0600

    Update

---
+++ switch.js
@@ -0,0 +1,44 @@
+var color = 'yellow';
+
+if(color == 'red') {
+    console.log('Color is Red');
+}
+else if (color == 'blue') {
+    console.log('Color is Blue');
+}
+else if (color == 'white') {
+    console.log('Color is white');
+}
+else if (color == 'purple') {
+    console.log('Color is Purple');
+}
+else if (color == 'megenda') {
+    console.log('Color is Megenda');
+}
+else if (color == 'yellow') {
+    console.log('Color is yellow');
+}
+else if (color == 'black') {
+    console.log('Color is Black');
+}
+
+switch(color){
+    case 'blue':
+    console.log('Color is blue');
+    break;
+    case 'red':
+    console.log('Color is Red');
+    break;
+    case 'Green':
+    console.log('Color is Green');
+    break;
+    case 'purple':
+    console.log('Color is Purple');
+    break;
+    case 'yellow':
+    console.log('Color is Yellow');
+    break;
+    case 'black':
+    console.log('Color is Black');
+    break;
+}
